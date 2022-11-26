82b10bd01a8b8f607e747dc8f109c43518cff13b
Ahmed Maher <65620946+Ahmed336-cell@users.noreply.github.com>
2022-11-26 18:40:09 +0200

    Create E.cpp

---
+++ problem 5/E.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x = 0 ,n;
+	string s;
+	cin >> n;
+
+	while (n--) {
+		cin >> s;
+		if (s[1] == '+') {
+			x++;
+		}
+		else {
+			x--;
+		}
+	}
+
+	
+
+	cout << x << endl;
+}
