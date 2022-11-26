22fb633964682b091b3c263a75214af5e63c1019
Ahmed Maher <65620946+Ahmed336-cell@users.noreply.github.com>
2022-11-26 17:55:06 +0200

    Create B.cpp

---
+++ problem 2/B.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// using ascii table
+// a =97 , b = 98 .....
+int main()
+{
+	int p;
+	string s;
+	for (int i = 0; i <26; i++) {
+		cin >>p ;
+		s = p + 96;
+		cout << s;
+	}
+}
